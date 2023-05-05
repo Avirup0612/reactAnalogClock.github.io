d5f18b52354292916196bfa15921acd357127620
Avirup0612 <mitraavirup1@gmail.com>
2023-05-05 17:20:39 +0530

    update data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import Appdark from './Appdark';
-import Applight from './Applight';
 import React, { useState } from 'react';
+import './lightstyle.css';
+import './darkstyle.css';
 
 
 const App = () => {
 
-    const[currTheme, setTheme] = useState("dark");
+    const[currTheme, setTheme] = useState(true);
+    const[hr, setHr] = useState(0);
+    const[min, setMin] = useState(0);
+    const[sec, setSec] = useState(0);
+
+    setInterval(() => {
+        let d = new Date();
+        let hourTime= d.getHours();
+        let minTime= d.getMinutes();
+        let secTime= d.getSeconds();
+        setHr(30*hourTime+(minTime/2));
+        setMin(6*minTime);
+        setSec(6*secTime);
+      }, 1000);
+    
 
     const btnStyledark = {
         position: "absolute",
@@ -42,32 +56,27 @@ const App = () => {
     const[btnStyle, setBtnStyle] = useState(btnStyledark);
 
     const toggleTheme = () => {
-        if(currTheme==="dark") {
+        if(currTheme===true) {
             setBtnStyle(btnStylelight);
-            setTheme("light");
-        }else if(currTheme==="light") {
+            setTheme(false);
+        }else if(currTheme===false) {
             setBtnStyle(btnStyledark);
-            setTheme("dark");
+            setTheme(true);
         }
     }
 
-    console.log(currTheme);
-
-    if(currTheme==="dark") {
-        return(
-            <>
-            <button className='togglebtn' onClick={toggleTheme} style={btnStyle}><ion-icon name="sunny-outline"></ion-icon></button>
-            <Appdark/> 
-            </>
-        )
-    }else if(currTheme==="light") {
-    return(
+    return (
         <>
-        <button className='togglebtn' onClick={toggleTheme} style={btnStyle}><ion-icon name="moon-outline"></ion-icon></button>
-        <Applight/> 
+          <button className='togglebtn' onClick={toggleTheme} style={btnStyle}><ion-icon name={currTheme ? 'sunny-outline':'moon-outline'}></ion-icon></button>
+          <div className={currTheme ? 'mainpagedark':'mainpagelight'}>
+            <div className={currTheme ? 'clockdark':'clocklight'}>
+              <div className={currTheme ? 'hourdark':'hourlight'} style={{transform:`rotate(${hr}deg)`}}></div>
+              <div className={currTheme ? 'minutedark':'minutelight'} style={{transform:`rotate(${min}deg)`}}></div>
+              <div className={currTheme ? 'seconddark':'secondlight'} style={{transform:`rotate(${sec}deg)`}}></div>
+            </div>
+          </div>
         </>
-    )
-    }
+      );
 }
 
 export default App;
